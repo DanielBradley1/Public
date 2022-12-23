a67536ec30131ea3ec091cec2e197ae6d77f8b5f
Daniel Bradley <96792909+DanielBradley1@users.noreply.github.com>
2022-12-23 06:43:57 +0000

    Create CreateaCustomIntuneRole.ps1

---
+++ Microsoft Intune PowerShell Scripts/CreateaCustomIntuneRole.ps1
@@ -0,0 +1,72 @@
+####
+#Name: Daniel Bradley
+#LinkedIn: https://www.linkedin.com/in/danielbradley2/
+#Description: This script will automatically check if your MDM user scope is configured. If it is set to none, it will assign all users to the scope.
+####
+
+###
+$DisplayName = "My Custom Role2" #Define your role name
+$RoleDescription = "My custom role description2" #Define the role description
+###
+
+#Import module
+Import-Module Microsoft.Graph.DeviceManagement.Administration
+
+#Select beta profile
+Select-MgProfile -Name Beta
+
+#Connect to Microsoft Graph
+Connect-MgGraph -scopes DeviceManagementRBAC.ReadWrite.All
+
+#Store the URI path
+$uri = "https://graph.microsoft.com/beta/deviceManagement/roleDefinitions/"
+
+#Store Json Payload
+$json = @'
+		{
+    "@odata.context": "https://graph.microsoft.com/beta/$metadata#deviceManagement/roleDefinitions/$entity",
+    "@odata.type": "#microsoft.graph.deviceAndAppManagementRoleDefinition",
+    "displayName": "<rname>",
+    "description": "<rdesc>",
+    "isBuiltInRoleDefinition": true,
+    "isBuiltIn": true,
+    "roleScopeTagIds": [],
+    "permissions": [
+        {
+            "actions": [
+                "Microsoft.Intune_Organization_Read",
+            ],
+            "resourceActions": [
+                {
+                    "allowedResourceActions": [
+                        "Microsoft.Intune_ManagedDevices_Read",
+                    ],
+                    "notAllowedResourceActions": []
+                }
+            ]
+        }
+    ],
+    "rolePermissions": [
+        {
+            "actions": [
+                "Microsoft.Intune_ManagedDevices_Read",
+            ],
+            "resourceActions": [
+                {
+                    "allowedResourceActions": [
+                        "Microsoft.Intune_ManagedDevices_Read",
+                    ],
+                    "notAllowedResourceActions": []
+                }
+            ]
+        }
+    ]
+}
+'@
+
+#Update unique payload information
+$json = $json -replace '<rname>',$DisplayName
+$json = $json -replace '<rdesc>',$RoleDescription
+
+#Create custom Intune role
+Invoke-MgGraphRequest -Uri $uri -body $json -Method POST -ContentType "application/json"
