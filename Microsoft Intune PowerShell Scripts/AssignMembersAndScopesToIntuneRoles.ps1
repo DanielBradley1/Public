27b018e2840c6311a8af744ffe79d145407c770f
Daniel Bradley <96792909+DanielBradley1@users.noreply.github.com>
2022-12-23 06:43:22 +0000

    Create AssignMembersAndScopesToIntuneRoles.ps1

---
+++ Microsoft Intune PowerShell Scripts/AssignMembersAndScopesToIntuneRoles.ps1
@@ -0,0 +1,47 @@
+###
+$DisplayName = "My Display Name" #Define a name for your role assignment
+$AdminGroupName = "Approval Admins" #Define the group that contains the user you wish you assign this role to
+$RoleName = "Read-Only Device Role" #Define the name of the active role
+###
+####
+#Name: Daniel Bradley
+#LinkedIn: https://www.linkedin.com/in/danielbradley2/
+#Description: This script will automatically check if your MDM user scope is configured. If it is set to none, it will assign all users to the scope.
+####
+
+#Import Module
+Import-Module Microsoft.Graph.DeviceManagement.Administration
+
+#Select beta profile
+Select-MgProfile -Name Beta
+
+#Connect to Microsoft Graph
+Connect-MgGraph -scopes DeviceManagementRBAC.ReadWrite.All
+
+$Role = Get-MgDeviceManagementRoleDefinition | Where-Object {$_.DisplayName -eq $Rolename}
+$admingroup = Get-MgGroup | Where-Object {$_.DisplayName -eq $AdminGroupName} | select id
+
+#Store URI path
+$uri = "https://graph.microsoft.com/beta/deviceManagement/roleAssignments"
+
+#Store Json payload
+$json = @'
+{
+"id":"",
+"description":"",
+"displayName":"<dname>",
+"members":["<admgroup>"],
+"resourceScopes":[],
+"roleDefinition@odata.bind":"https://graph.microsoft.com/beta/deviceManagement/roleDefinitions('<roleid>')",
+"scopeType":"allDevices"
+}
+'@
+
+#Update unique payload information.
+$json = $json -replace '<roleid>',$Role.id
+$json = $json -replace '<dname>',$DisplayName
+$json = $json -replace '<admgroup>',$admingroup.id
+
+
+#Create role assignment
+Invoke-MgGraphRequest -Uri $uri -body $json -Method POST -ContentType "application/json"
